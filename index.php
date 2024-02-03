0abb2f5b0c7a8057c01a3c9afe8c2514ccdd98ea
Giulia Granzotto <150152064+giuliagranzotto@users.noreply.github.com>
2024-02-03 21:54:34 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,309 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Digital Marketing Conference</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+  </head>
+  <body>
+    <!-- Inizio Sezione 1 -->
+	<nav class="navbar navbar-expand-lg bg-primary sticky-top" data-bs-theme="dark">
+	  <div class="container">
+		<a class="navbar-brand" href="index.php">DM Conference</a>
+		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		  <span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+		  <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+			<li class="nav-item">
+			  <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+			</li>
+			<li class="nav-item">
+			  <a class="nav-link" href="#esperti">Esperti</a>
+			</li>
+			<li class="nav-item">
+			  <a class="nav-link" href="#about">About</a>
+			</li>
+		  </ul>
+		</div>
+	  </div>
+	</nav>
+	<!-- Fine Sezione 1 -->
+	
+	<!-- Inizio Sezione 2 -->
+	<div id="jumbotron" class="container-fluid text-white text-md-center">
+		<h1 class="display-4">Digital Marketing Conference</h1>
+		<p class="lead">La conferenza sul Digital Marketing numero 1 in Italia</p>
+		<div class="btn-group" role="group" aria-label="Basic example">
+		  <button type="button" class="btn btn-primary btn-lg" data-bs-toggle="modal" data-bs-target="#registrazione">Registrati</button>
+		  <a class="btn btn-light btn-lg" href="#esperti" role="button">Gli esperti</a>
+		</div>
+	</div>
+	<!-- Fine Sezione 2 -->
+    
+	<!-- Inizio Sezione 3 -->
+	<div class="container text-center pt-4">
+	  <div class="row">
+		<div class="col-md-3">
+		  <h3>Gli esperti</h3>
+
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+
+          <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</div>
+		<div class="col-md-6">
+		  <h3>Il Digital Marketing</h3>
+		  
+		  <img src="img/marketing.jpg" class="img-fluid rounded" alt="Digital Marketing">
+
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+
+          <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</div>
+		<div class="col-md-3">
+		  <h3>Cosa imparerai</h3>
+		  
+		  <div class="accordion" id="accordionPanelsStayOpenExample">
+			  <div class="accordion-item">
+				<h5 class="accordion-header">
+				  <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+					Google Ads
+				  </button>
+				</h5>
+				<div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+				  <div class="accordion-body">
+					Lorem ipsum dolor
+				  </div>
+				</div>
+			  </div>
+			  <div class="accordion-item">
+				<h5 class="accordion-header">
+				  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+					Facebook Adv
+				  </button>
+				</h5>
+				<div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+				  <div class="accordion-body">
+					Dolor sit amet
+				  </div>
+				</div>
+			  </div>
+			  <div class="accordion-item">
+				<h5 class="accordion-header">
+				  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+					SEO
+				  </button>
+				</h5>
+				<div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+				  <div class="accordion-body">
+					Lorem ipsum dolor
+				  </div>
+				</div>
+			  </div>
+			  <div class="accordion-item">
+				<h5 class="accordion-header">
+				  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+					Growth Hacking
+				  </button>
+				</h5>
+				<div id="collapseFour" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+				  <div class="accordion-body">
+					Dolor sit amet
+				  </div>
+				</div>
+			  </div>		  
+		  </div>		
+		</div>
+	  </div>
+	</div>
+	<!-- Fine Sezione 3 -->
+	
+	<!-- Inizio Sezione 4 -->
+	<div id="esperti" class="container text-center">
+	 <h2 class="mb-5">Gli esperti</h2>
+	  <div class="row">
+		<div class="col-6 col-md-4 col-lg-3">
+			<div class="card">
+			  <img src="img/amalia.png" class="card-img-top" alt="Amalia Freglioje">
+			  <div class="card-body">
+				<h4 class="card-title">Amalia Freglioje</h4>
+				<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+			  </div>
+			</div>
+		</div>
+		
+		<div class="col-6 col-md-4 col-lg-3">
+			<div class="card">
+			  <img src="img/pinco.png" class="card-img-top" alt="Pinco Pallo">
+			  <div class="card-body">
+				<h4 class="card-title">Pinco Pallo</h4>
+				<p class="card-text">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.</p>
+			  </div>
+			</div>
+		</div>
+
+		<div class="col-6 col-md-4 col-lg-3">
+			<div class="card">
+			  <img src="img/anna.png" class="card-img-top" alt="Anna Pannocchia">
+			  <div class="card-body">
+				<h4 class="card-title">Anna Pannocchia</h4>
+				<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+			  </div>
+			</div>
+		</div>
+		
+		<div class="col-6 col-md-4 col-lg-3">
+			<div class="card">
+			  <img src="img/pinco2.png" class="card-img-top" alt="Pinco Pallino">
+			  <div class="card-body">
+				<h4 class="card-title">Pinco Pallino</h4>
+				<p class="card-text">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.</p>
+			  </div>
+			</div>
+		</div>	
+
+		<div class="col-6 col-md-4 col-lg-3">
+			<div class="card">
+			  <img src="img/herbert.png" class="card-img-top" alt="Herbert Ballerina">
+			  <div class="card-body">
+				<h4 class="card-title">Herbert Ballerina</h4>
+				<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+			  </div>
+			</div>
+		</div>
+		
+		<div class="col-6 col-md-4 col-lg-3">
+			<div class="card">
+			  <img src="img/ciccio.png" class="card-img-top" alt="Ciccio Pasticcio">
+			  <div class="card-body">
+				<h4 class="card-title">Ciccio Pasticcio</h4>
+				<p class="card-text">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.</p>
+			  </div>
+			</div>
+		</div>			
+	  </div>
+	</div>
+	<!-- Fine Sezione 4 -->
+	
+	<!-- Inizio Sezione 5 -->
+	<div id="about" class="container"> 
+	   <div class="row py-4 text-muted">
+		  <div class="col-md col-xl-5">
+			 <p><strong>About Lacerba</strong></p>
+			 <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo</p>
+		  </div>
+		  <div class="col-md col-xl-5 ms-auto">
+			 <p><strong>Vuoi ricevere la newsletter?</strong></p>
+			 <div class="input-group">
+				<input type="text" class="form-control" placeholder="Email">
+				<span class="input-group-btn">
+				   <button class="btn btn-primary" type="button">Iscriviti</button>
+				</span>
+			 </div>
+		  </div>
+	   </div>
+	</div>
+	<!-- Fine Sezione 5 -->
+	
+	<!-- Inizio Sezione 6 -->
+	<div class="container py-4">
+	  <div class="row align-items-center">
+		<div class="col-md-7">
+		  <ul class="nav justify-content-center justify-content-md-start">
+			  <li class="nav-item">
+				<a class="nav-link" href="https://www.facebook.com/lacerba.io/?locale=it_IT" target="_blank">Community</a>
+			  </li>
+			  <li class="nav-item">
+				<a class="nav-link" href="https://lacerba.io/" target="_blank">Lacerba.io</a>
+			  </li>
+				<li class="nav-item">
+				<!-- Split dropup button -->
+				 <div class="btn-group dropup">
+					<button type="button" class="btn btn-secondary">
+						Scopri di più
+					</button>
+					<button type="button" class="btn btn-secondary dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
+						<span class="visually-hidden">Toggle Dropdown</span>
+					</button>
+					<ul class="dropdown-menu">
+						 <li><a class="dropdown-item" href="https://www.talentform.it/index.php/formazione-professionale/digital-marketing-e-digital-advertising-corso-gratuito-a-distanza-in-aula-virtuale-edizione-del-30-marzo-2023/" target="_blank">Corso Digital Marketing</a></li>
+						 <li><a class="dropdown-item" href="https://www.24orebs.com/edu/marketing?campaignid=19647534207&keyword=corso%20web%20marketing&gclid=CjwKCAjwoIqhBhAGEiwArXT7K_MXb9aqLWF1WSgOL57qnqO44-g5IXHm1wemysbTz7L-OeXjFtswGxoC-gsQAvD_BwE" target="_blank">Corso Web Marketing</a></li>
+						 <li><a class="dropdown-item" href="https://marketingdigitale.info/corso-social-media-manager/" target="_blank">Corso Social Media Marketing</a></li>
+					</ul>				
+			   </div>
+			 </li>
+		  </ul>
+		  
+		</div>
+		<div class="col-md-5 text-center text-md-end">
+		  &copy; 2023 Digital Marketing Conference & Lacerba
+		</div>
+	  </div>
+	</div>
+	<!-- Fine Sezione 6 -->
+	
+	<!-- Inizio collegamento database -->
+	<?php
+	if(isset($_POST["invio"])){
+		
+		$nome = $_POST["nome"];
+		$cognome = $_POST["cognome"];
+		$email = $_POST["email"];
+		
+		$connessioneDB = mysqli_connect("localhost", "root", "", "registrazione_dmconference");
+		
+		if(!$connessioneDB){
+			die("Ooops, qualcosa è andato storto");
+		}
+		
+		mysqli_query($connessioneDB, "INSERT INTO iscritti(Nome, Cognome, Email) VALUES('$nome', '$cognome', '$email')");
+		
+		mysqli_close($connessioneDB);
+	}
+	?>
+	<!-- Fine collegamento database -->
+	
+	<!-- Inizio modale -->
+	<div class="modal fade" id="registrazione" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	  <div class="modal-dialog modal-dialog-centered">
+		<div class="modal-content">
+		  
+		  <div class="modal-header">
+			<h5 class="modal-title fs-5" id="exampleModalLabel">Registrati all'evento</h5>
+			<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+		  </div>
+		  
+		  <div class="modal-body">
+			<form method="post">
+				
+				<div class="row pb-4">
+				  <div class="col">
+					<input type="text" class="form-control" placeholder="Nome" aria-label="Nome" name="nome">
+				  </div>
+				  <div class="col">
+					<input type="text" class="form-control" placeholder="Cognome" aria-label="Cognome" name="cognome">
+				  </div>
+				</div>
+				
+				<div class="row">
+				  <div class="col">
+				    <label for="exampleInputEmail1" class="form-label">Email</label>
+					<input type="text" class="form-control" aria-label="Email" name="email">
+					<div id="emailHelp" class="form-text">Non condividere con altri la tua e-mail.</div>
+				  </div>
+				</div>
+				<div class="modal-footer">
+					<button type="submit" class="btn btn-primary" name="invio">Invia</button>
+				</div>				
+			</form>		  
+		  </div>
+		</div>
+	  </div>
+	</div>
+	<!-- Fine modale -->
+    
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+  </body>
+</html>
